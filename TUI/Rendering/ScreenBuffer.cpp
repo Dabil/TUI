2d8e9150f2e116cdf004ce8615633c0953aede3d
Daniel K Billett Jr <dbillett1@gmail.com>
2026-03-16 01:01:42 -0400

    ScreenBuffer.cpp: Implementation Skeleton
    Adds:
    - ScreenBuffer.cpp
    
    This is the skeleton for the new ScreenBuffer implementation that will be built out in 2.3.

---
+++ TUI/Rendering/ScreenBuffer.cpp
@@ -0,0 +1,81 @@
+#include "Rendering/ScreenBuffer.h"
+
+ScreenBuffer::ScreenBuffer()
+{
+
+}
+
+ScreenBuffer::ScreenBuffer(int width, int height)
+{
+
+}
+
+void ScreenBuffer::resize(int width, int height)
+{
+
+}
+
+void ScreenBuffer::clear(const Style& style = Style())
+{
+
+}
+
+int ScreenBuffer::getWidth() const
+{
+
+}
+
+int ScreenBuffer::getHeight() const
+{
+
+}
+
+bool ScreenBuffer::inBounds(int x, int y) const
+{
+
+}
+
+const ScreenCell& ScreenBuffer::getCell(int x, int y) const
+{
+
+}
+
+ScreenCell& ScreenBuffer::getCell(int x, int y)
+{
+
+}
+
+void ScreenBuffer::setCell(int x, int y, const ScreenCell& cell)
+{
+
+}
+
+void ScreenBuffer::writeChar(int x, int y, char glyph, const Style& style)
+{
+
+}
+
+void ScreenBuffer::writeString(int x, int y, const std::string& text, const Style& style)
+{
+
+}
+
+void ScreenBuffer::fillRect(const Rect& rect, char glyph, const Style& style)
+{
+
+}
+
+void ScreenBuffer::drawFrame(const Rect& rect, const Style& style)
+{
+
+}
+
+std::string ScreenBuffer::renderToString() const
+{
+
+}
+
+int ScreenBuffer::index(int x, int y) const
+{
+
+}
