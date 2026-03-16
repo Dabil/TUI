77d659b7d2c5cc5ac5ae1438ff842ea45cde642d
Daniel K Billett Jr <dbillett1@gmail.com>
2026-03-16 12:57:35 -0400

    Surface: Implementation skeleton for the new wrapper.
    Adds:
    - Surface.cpp

---
+++ TUI/Rendering/Surface.cpp
@@ -0,0 +1,28 @@
+#include "Rendering/Surface.h"
+
+Surface::Surface() = default;
+
+Surface::Surface(int width, int height)
+{
+
+}
+
+void Surface::resize(int width, int height)
+{
+
+}
+
+void Surface::clear(const Style& style)
+{
+
+}
+
+ScreenBuffer& Surface::buffer()
+{
+
+}
+
+const ScreenBuffer& Surface::buffer() const
+{
+
+}
