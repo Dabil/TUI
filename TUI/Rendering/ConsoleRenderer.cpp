3f13426b0af1541a89bed5e6a9a7317b76ba70fc
Daniel K Billett Jr <dbillett1@gmail.com>
2026-03-17 10:44:14 -0400

    ConsoleRenderer: initialize() method:
    Modifies:
    ConsoleRenderer.cpp
    
    Capture original state so we can restore everything on shutdown. Then sets up the console so it's ready for rendering.
    
    - get console handles
    - configure output modes
    - enable UTF-8
    - set input behavior
    - prepare internal state

--- TUI/Rendering/ConsoleRenderer.cpp
+++ TUI/Rendering/ConsoleRenderer.cpp
@@ -15,7 +15,74 @@ ConsoleRenderer::~ConsoleRenderer()
 
 bool ConsoleRenderer::initialize() override
 {
+    if (m_initialized)
+    {
+        return true;
+    }
 
+    m_hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    m_hIn = GetStdHandle(STD_INPUT_HANDLE);
+
+    if (m_hOut == INVALID_HANDLE_VALUE || m_hOut == nullptr)
+    {
+        return false;
+    }
+
+    if (m_hIn == INVALID_HANDLE_VALUE || m_hIn == nullptr)
+    {
+        return false;
+    }
+
+    CONSOLE_SCREEN_BUFFER_INFO info{};
+    if (!GetConsoleScreenBufferInfo(m_hOut, &info))
+    {
+        return false;
+    }
+
+    m_defaultAttributes = info.wAttributes;
+    m_originalOutputCodePage = GetConsoleOutputCP();
+    m_originalInputCodePage = GetConsoleCP();
+
+    DWORD outMode = 0;
+    if (GetConsoleMode(m_hOut, &outMode))
+    {
+        m_originalOutputMode = outMode;
+        m_haveOriginalOutputMode = true;
+    }
+
+    DWORD inMode = 0;
+    if (GetConsoleMode(m_hIn, &inMode))
+    {
+        m_originalInputMode = inMode;
+        m_haveOriginalInputMode = true;
+    }
+
+    CONSOLE_CURSOR_INFO cursorInfo{};
+    if (GetConsoleCursorInfo(m_hOut, &cursorInfo))
+    {
+        m_cursorWasVisible = (cursorInfo.bVisible != FALSE);
+    }
+
+    if (!configureConsole())
+    {
+        restoreConsoleState();
+        return false;
+    }
+
+    if (!queryVisibleConsoleSize(m_consoleWidth, m_consoleHeight))
+    {
+        m_consoleWidth = 120;
+        m_consoleHeight = 40;
+    }
+
+    m_previousFrame.resize(m_consoleWidth, m_consoleHeight);
+    m_previousFrame.clear();
+
+    m_currentStyle = Style{};
+    m_firstPresent = true;
+    m_initialized = true;
+
+    return true;
 }
 
 void ConsoleRenderer::shutdown() override
