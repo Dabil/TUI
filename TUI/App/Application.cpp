49c03c17a208fb65ecafea445ecbb4ec4fad4f26
Daniel K Billett Jr <dbillett1@gmail.com>
2026-03-17 17:55:06 -0400

    Application: class implementation skeleton
    Adds:
    - Application.cpp
    
    Skeleton for the class implementation for Application.

---
+++ TUI/App/Application.cpp
@@ -0,0 +1,38 @@
+#include "APP/Application.h"
+
+Application::Application() = default;
+
+Application::~Application()
+{
+
+}
+
+bool Application::initialize()
+{
+
+}
+
+void Application::run()
+{
+
+}
+
+void Application::shutdown()
+{
+
+}
+
+void Application::handleResize()
+{
+
+}
+
+void Application::update(double deltaTime)
+{
+
+}
+
+void Application::render()
+{
+
+}
