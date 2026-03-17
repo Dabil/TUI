7421fe0e00eabbc3045a752d873bacdd8c4c9dfc
Daniel K Billett Jr <dbillett1@gmail.com>
2026-03-17 19:08:29 -0400

    ScreenManager: implementation skeleton
    Adds:
    - ScreenManager.cpp
    
    implementation skeleton for the ScreenManager

---
+++ TUI/App/ScreenManager.cpp
@@ -0,0 +1,36 @@
+#include "App/ScreenManager.h"
+
+void ScreenManager::pushScreen(std::unique_ptr<Screen> screen)
+{
+
+}
+
+void ScreenManager::popScreen()
+{
+
+}
+
+void ScreenManager::clearScreens()
+{
+
+}
+
+bool ScreenManager::hasScreens() const
+{
+
+}
+
+Screen* ScreenManager::currentScreen()
+{
+
+}
+
+void ScreenManager::update(double deltaTime)
+{
+
+}
+
+void ScreenManager::drawCurrentScreen(Surface& surface)
+{
+
+}
