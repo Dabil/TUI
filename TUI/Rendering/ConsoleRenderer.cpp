d30da1f6300e157785572805d491fb8daab80598
Daniel K Billett Jr <dbillett1@gmail.com>
2026-03-17 07:08:14 -0400

    ConsoleRenderer: Implementation skeleton
    Adds:
    - ConsoleRenderer.cpp
    
    Skeleton for the implementation of the class. All methods are layed out so implementation work can begin.

---
+++ TUI/Rendering/ConsoleRenderer.cpp
@@ -0,0 +1,46 @@
+#include "ConsoleRenderer.h"
+
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+ConsoleRenderer::ConsoleRenderer() = default;
+
+ConsoleRenderer::~ConsoleRenderer() override
+{
+}
+
+bool ConsoleRenderer::initialize() override
+{
+
+}
+
+void ConsoleRenderer::shutdown() override
+{
+
+}
+
+void ConsoleRenderer::present(const ScreenBuffer& frame) override
+{
+
+}
+
+void ConsoleRenderer::resize(int width, int height) override
+{
+
+}
+
+int ConsoleRenderer::getConsoleWidth() const
+{
+
+}
+
+int ConsoleRenderer::getConsoleHeight() const
+{
+
+}
+
+bool ConsoleRenderer::pollResize()
+{
+
+}
