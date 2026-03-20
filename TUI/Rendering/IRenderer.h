55b271ab8292e0afef10d4f39300d99b96a5d3ae
Dan <dbillett1@gmail.com>
2026-03-20 10:14:44 -0400

    IRenderer: Unicode Readiness Upgrade
    Modifled:
    - IRenderer
    
    Added:
    textCapabilities()

--- TUI/Rendering/IRenderer.h
+++ TUI/Rendering/IRenderer.h
@@ -1,32 +1,7 @@
 #pragma once
 
-class ScreenBuffer;
-
-/*
-    IRenderer exists to separate the rendering engine from the specific
-    output system (console, SDLwindow, web terminal, etc)
-
-    IRenderer creates an abstraction layer between the TUI engine and 
-    the Rendering implementation. So this makes adding additional
-    backends clean and neat. 
-
-    With IRenderer the engine depends on the interface:
-    std::unique_ptr<IRenderer> renderer;
-
-    Then at runtime you decide which renderer to use:
-        renderer = std::make_unique<ConsoleRenderer>();
-        renderer = std::make_unique<SDLRenderer();
-        renderer = std::make_unique<WebRenderer();
-        etc...
-
-        The engine does not know or care what type of renderer is
-        behind it. The interface describes what a renderer must
-        be able to do. So any renderer must implement IRenderer 
-        functions.
-
-        Because the interface has been standardized, the engine code
-        doesn't change at all.The engine becomes platform independent. 
-*/
+#include "Rendering/ScreenBuffer.h"
+#include "Rendering/Text/TextTypes.h"
 
 class IRenderer
 {
@@ -36,10 +11,13 @@ public:
     virtual bool initialize() = 0;
     virtual void shutdown() = 0;
 
-    virtual void present(const ScreenBuffer& frame) = 0;
+    virtual void present(const ScreenBuffer& buffer) = 0;
+
     virtual void resize(int width, int height) = 0;
     virtual bool pollResize() = 0;
 
     virtual int getConsoleWidth() const = 0;
     virtual int getConsoleHeight() const = 0;
+
+    virtual TextBackendCapabilities textCapabilities() const = 0;
 };
