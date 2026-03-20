e1cdfe0f9e117d5860fa9c8a174c4f2dc9afbc68
Dan <dbillett1@gmail.com>
2026-03-20 13:10:17 -0400

    ShowcaseScreen.cpp: Implementation skeleton
    Adds:
    - ShowcaseScreen.cpp
    
    Simple implementation skeleton to get project to build fully for the first time ;)

---
+++ TUI/Screens/ShowcaseScreen.cpp
@@ -0,0 +1,41 @@
+#include "Screens/ShowcaseScreen.h"
+
+ShowcaseScreen::ShowcaseScreen()
+{
+
+}
+
+void ShowcaseScreen::onEnter()
+{
+
+}
+
+void ShowcaseScreen::update(double deltaTime)
+{
+
+}
+
+void ShowcaseScreen::draw(Surface& surface)
+{
+
+}
+
+void ShowcaseScreen::drawControlDeck(Surface& surface)
+{
+
+}
+
+void ShowcaseScreen::drawRetroTerminal(Surface& surface)
+{
+
+}
+
+void ShowcaseScreen::drawNeonDialog(Surface& surface)
+{
+
+}
+
+void ShowcaseScreen::drawOpsWall(Surface& surface)
+{
+
+}
