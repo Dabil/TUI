01ed300d45ee67d16d248ea330a95f10d919cb80
Dan <dbillett1@gmail.com>
2026-04-24 17:18:51 -0400

    WritePreset: Remove legacy write policies
    Modifies:
    - Rendering/Composition/WritePresets.h
    
    All legacy write policies have been replaced in call-sites, so these can be removed now

--- TUI/Rendering/Composition/WritePresets.h
+++ TUI/Rendering/Composition/WritePresets.h
@@ -8,10 +8,7 @@ namespace Composition::WritePresets
     WritePolicy visibleOnly(DepthPolicy depthPolicy = DepthPolicy::Ignore);
     WritePolicy authoredOnly(DepthPolicy depthPolicy = DepthPolicy::Ignore);
     WritePolicy solidObject(DepthPolicy depthPolicy = DepthPolicy::Ignore);
-    WritePolicy transparentOverlay(DepthPolicy depthPolicy = DepthPolicy::Ignore);
 
-    WritePolicy visibleObject(DepthPolicy depthPolicy = DepthPolicy::Ignore);
-    WritePolicy transparentGlyphOverlay(DepthPolicy depthPolicy = DepthPolicy::Ignore);
     WritePolicy glyphsOnly(DepthPolicy depthPolicy = DepthPolicy::Ignore);
     WritePolicy styleMask(DepthPolicy depthPolicy = DepthPolicy::Ignore);
     WritePolicy styleBlock(DepthPolicy depthPolicy = DepthPolicy::Ignore);
